aa17a5a5e0c5170a687f73a7b3603816ba301c90
Vladimir <vladimir94@gmail.com>
2017-11-10 10:19:37 +0100

    --

---
+++ isok/src/main/java/isok/isok/IsokApplication.java
@@ -0,0 +1,12 @@
+package isok.isok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsokApplication.class, args);
+	}
+}
---
+++ isok/src/test/java/isok/isok/IsokApplicationTests.java
@@ -0,0 +1,16 @@
+package isok.isok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsokApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

9a6427c03026aeb8e7cfba605da1fadae6f4cf66
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2017-12-27 07:30:23 +0100

    ssl example between applications, Certificate included

--- isok/src/main/java/isok/isok/categoryFactor/CategoryFactorController.java
+++ isok/src/main/java/isok/isok/categoryFactor/CategoryFactorController.java
@@ -1,15 +1,33 @@
 package isok.isok.categoryFactor;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.ws.rs.BadRequestException;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,10 +44,6 @@ import model.dto.CategoryFactor;
 @RequestMapping("/api/categoryFactor")
 public class CategoryFactorController {
 
-	@Bean
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}
 	
 	@Value("${dataccessPort}")
 	private String dataccessPort;
@@ -67,5 +81,32 @@ public class CategoryFactorController {
 		} catch(HttpServerErrorException e) {
 			throw e;
 		}
+	}
+	
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		/*try {
+			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		    keyStore.load(new FileInputStream(new File("sep.p12")), "sep12345".toCharArray());
+	
+		    SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+		            new SSLContextBuilder()
+		                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+		                    .loadKeyMaterial(keyStore, "sep12345".toCharArray())
+		                    .build(),
+		            NoopHostnameVerifier.INSTANCE);
+	
+		    HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+	
+		    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		    RestTemplate restTemplate = new RestTemplate(requestFactory);
+			
+		    return restTemplate;
+		}
+		catch(Exception exc) {
+			return null;
+		}*/
+		return new RestTemplate();
 	}
 }

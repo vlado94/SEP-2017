5dfe0cd82c3ca8f24375ba046885aa6591295e04
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2018-01-24 18:39:03 +0100

    --

--- acquirer/src/main/webapp/acquirer-view/src/app/app.component.ts
+++ acquirer/src/main/webapp/acquirer-view/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'app';
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  submitCardDataForm(){
+		console.log("11111111111");
+	}
+  
+  
+  
 }

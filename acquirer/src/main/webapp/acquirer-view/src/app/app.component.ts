68c82dad65395c831a95d44f2204fd4f92ddc60c
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2018-01-25 21:38:10 +0100

    Accepting parameters from external app

--- acquirer/src/main/webapp/acquirer-view/src/app/app.component.ts
+++ acquirer/src/main/webapp/acquirer-view/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Http, Response } from "@angular/http";
+import { Http, Response, URLSearchParams } from "@angular/http";
 import { PaymentRequest } from './payment-request';
 import { AppService } from './app.service';
+
 
 @Component({
   selector: 'app-root',
@@ -28,6 +29,21 @@ export class AppComponent implements OnInit {
 			expDate: new FormControl('', [Validators.required]),
 			cvv2: new FormControl('', [Validators.required]),
 		});
+		
+  		var normalizedQueryString = "";
+  		if (window.location.search.indexOf('?') == 0)
+  		{ normalizedQueryString = window.location.search.substring(1); } 
+  		else 
+  		{ normalizedQueryString = window.location.search; } 
+  		let params = new URLSearchParams(normalizedQueryString);
+  		
+  		let id = params.get('id');
+  		let price = params.get('price');
+  		
+		console.log("ide paerama");
+		console.log(id);
+		console.log(price);
+		console.log("ide paerama");
   }
   
   createForm() {

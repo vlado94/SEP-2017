f6a625cec6a1f9b7dc5b9bebf63d042d1861d467
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2018-01-26 00:36:03 +0100

    payment services

---
+++ acquirer/src/main/java/com/sep/acquirer/bank/BankRepository.java
@@ -0,0 +1,8 @@
+package com.sep.acquirer.bank;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+public interface BankRepository extends PagingAndSortingRepository<Bank, Long>  {
+
+}
